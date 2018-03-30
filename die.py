4164a63b0dcfc31a40406e16ea33867d6fc35be1
John Vivian <jtvivian@gmail.com>
2018-03-30 04:07:18 -0700

    Refactor for dynamic example

--- die.py
+++ die.py
@@ -2,20 +2,20 @@ import pandas as pd
 
 columns = ['hit', 'special', 'defense', 'crit', 'crit-defense']
 die = {}
-die['red'] = pd.DataFrame([[0,0,0,1,0], [1,1,0,0,0], [1,1,0,0,0],
+die['Red'] = pd.DataFrame([[0,0,0,1,0], [1,1,0,0,0], [1,1,0,0,0],
                     [1,0,0,0,0], [2,1,0,0,0], [0,0,0,0,0]], columns=columns)
 
-die['green'] = pd.DataFrame([[0,0,0,0,1], [0,1,2,0,0], [0,1,1,0,0],
+die['Green'] = pd.DataFrame([[0,0,0,0,1], [0,1,2,0,0], [0,1,1,0,0],
                       [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]], columns=columns)
 
-die['orange'] = pd.DataFrame([[2,1,0,0,0], [1,0,0,0,0], [0,1,1,0,0],
+die['Orange'] = pd.DataFrame([[2,1,0,0,0], [1,0,0,0,0], [0,1,1,0,0],
                        [1,1,1,0,0], [0,0,1,0,0], [0,0,0,0,0]], columns=columns)
 
-die['blue'] = pd.DataFrame([[0,1,2,0,0], [1,1,1,0,0], [1,0,1,0,0],
+die['Blue'] = pd.DataFrame([[0,1,2,0,0], [1,1,1,0,0], [1,0,1,0,0],
                      [0,0,1,0,0], [1,1,0,0,0], [0,0,0,0,0]], columns=columns)
 
-die['yellow'] = pd.DataFrame([[1,1,1,0,0], [1,2,0,0,0], [0,1,1,0,0], 
+die['Yellow'] = pd.DataFrame([[1,1,1,0,0], [1,2,0,0,0], [0,1,1,0,0], 
                        [0,1,0,0,0], [0,1,0,0,0], [0,0,0,0,0]], columns=columns)
 
-die['black'] = pd.DataFrame([[0,0,1,0,0], [0,0,1,0,0], [0,0,1,0,0],
+die['Black'] = pd.DataFrame([[0,0,1,0,0], [0,0,1,0,0], [0,0,1,0,0],
                       [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]], columns=columns)
--- die_widget.py
+++ die_widget.py
@@ -4,15 +4,12 @@ die_n = {'Black': 3,
         'Orange': 3,
         'Blue': 3,
         'Red': 1,
-        'Green': 1}
+        'Green': 1,
+        'Yellow': 3}
 
-die_r = {x: RadioButtons(options=range(die_n[x] + 1), description=x) 
-               for x in die_n.keys()}
+def create_die_widgets():
+    return  {x: RadioButtons(options=range(die_n[x] + 1), description=x) 
+             for x in die_n.keys()}
 
-bs = VBox([die_r[x] for x in die_r.keys() if 'Blue' in x])
-ks = VBox([die_r[x] for x in die_r.keys() if 'Black' in x])
-os = VBox([die_r[x] for x in die_r.keys() if 'Orange' in x])
-r = VBox([die_r[x] for x in die_r.keys() if 'Red' in x])
-g = VBox([die_r[x] for x in die_r.keys() if 'Green' in x])
-
-output = HBox([bs, ks, os, r, g])
+def display_widgets(d):
+    return HBox([y for x, y in sorted(d.iteritems())])
