cc7b0d469288cf37348ebb274f4dee4b89721e68
John Vivian <jtvivian@gmail.com>
2018-03-31 04:47:47 -0700

    Add expected value and PMF

--- die.py
+++ die.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-columns = ['hit', 'special', 'defense', 'crit', 'crit-defense']
+columns = ['hit', 'special', 'defense', 'crit', 'crit_defense']
 die = {}
 die['Red'] = pd.DataFrame([[0,0,0,1,0], [1,1,0,0,0], [1,1,0,0,0],
                     [1,0,0,0,0], [2,1,0,0,0], [0,0,0,0,0]], columns=columns)
