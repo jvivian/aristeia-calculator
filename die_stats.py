7495dddc436e7d56d07074e9484bed3981fa7fe2
John Vivian <jtvivian@gmail.com>
2018-05-28 16:23:23 -0700

    Refactor for other's use

--- die_stats.py
+++ die_stats.py
@@ -4,27 +4,63 @@ from collections import Counter
 import pandas as pd
 import numpy as np
 
-def simulate_dists(die, n_rolls=10000):
-    """Return percentages for each possible die value"""
-    # Define dictionary for counting values
+def roll_die_pool(die, counts):
     dists = defaultdict(list)
+    for choice, num_die in counts.iteritems():
+        for _ in xrange(num_die):
+            for glyph, value in die[choice].iloc[np.random.randint(0, 6)].iteritems():
+                dists[glyph].append(value)
+    return pd.DataFrame(dists)
+
+def rolls_expected_value(rolls):
+    return pd.concat(rolls).sum().divide(len(rolls))
+
+def rolls_to_dists(rolls):
+    return pd.concat([x.sum() for x in rolls], axis=1).T
+
+def roll(die, attacker, defender, n_rolls=1000):
+    # TODO: Add dynamic roll storage. Precheck for hash here, otherwise run algorithm
+
+    # Objects to hold counts
+    a_rolls, d_rolls = [], []
+    t_total = defaultdict(list)
+    output = {}  # Final output containing dists
+    
+    # Convert attacker / defender widgets to die counts
+    a_counts = {x: y.value for x, y in attacker.iteritems()}
+    d_counts = {x: y.value for x, y in defender.iteritems()}
+    
+    # Simulate n_rolls
     for _ in xrange(n_rolls):
-        # Add value if present
-        for glyph, value in die.iloc[np.random.randint(0, 6)].iteritems():
-            dists[glyph].append(value)
-    return dists
+        
+        # Roll attacker pool
+        a_pool = roll_die_pool(die, a_counts)
+        a_raw_pool = a_pool.copy()  # We want to record an unadulterated set for attacker distributions
+        
+        # Roll defender pool
+        d_pool = roll_die_pool(die, d_counts)
 
-def counts_to_probabilities(counts):
-    n = len(counts.values()[0]) # Grab length from random count value
-    # Convert v in each k, v pair to a Counter object 
-    counts = {k: dict(Counter(v)) for k, v in counts.iteritems()}
-    # Normalize each count by number of counts    
-    counts = {k: {x: float(y) / n for x, y in v.iteritems()} for k, v in counts.iteritems()}
-    return pd.DataFrame(counts).replace(np.nan, 0)
-
-def hash_die_probabilties(die, n_rolls=10000, hdf_path='./die-probs.hdf'):
-    """Store simulated die probabilities in HDF file"""
-    for d, df in die.iteritems():
-        dists = simulate_dists(die[d], n_rolls=n_rolls)
-        probs = counts_to_probabilities(dists)
-        probs.to_hdf(hdf_path, key=d)
+        # Calculate total diffs
+        a = pd.Series({x: sum(y) for x, y in a_pool.iteritems()})
+        d = pd.Series({x: sum(y) for x, y in d_pool.iteritems()})
+        
+        # If crit-defense die, drop roll with highest number of hits
+        # TODO: Improve logic for removing most glyphs if tie
+        # Doesn't handle crit attack die
+        if d.crit_defense:
+            a_pool = a_pool.drop(a_pool.hit.argmax())
+            a = pd.Series({x: sum(y) for x, y in a_pool.iteritems()})
+        
+        a_diff = a.hit - d.defense if (a.hit - d.defense) > 0 else 0
+        d_diff = d.hit - a.defense if (d.hit - a.defense) > 0 else 0
+        
+        # Add crits, which are unaffected by defense
+        a_diff += a.crit
+        
+        # Update totals
+        a_rolls.append(a_raw_pool.sum())
+        d_rolls.append(d_pool.sum())
+        t_total['a'].append(a_diff)
+        t_total['d'].append(d_diff)
+        
+    return pd.concat(a_rolls, axis=1).T, pd.concat(d_rolls, axis=1).T, t_total
--- die_widget.py
+++ die_widget.py
@@ -1,4 +1,4 @@
-from ipywidgets import HBox, VBox, RadioButtons
+from ipywidgets import HBox, VBox, RadioButtons, Accordion
 
 die_n = {'Black': 3,
         'Orange': 3,
@@ -13,3 +13,16 @@ def create_die_widgets():
 
 def display_widgets(d):
     return HBox([y for x, y in sorted(d.iteritems())])
+
+def aristeia_dice_widget():
+    a = create_die_widgets()
+    d = create_die_widgets()
+    accordion = Accordion(children=[display_widgets(a), display_widgets(d)])
+    accordion.set_title(0, 'Attacker')
+    accordion.set_title(1, 'Defender')
+    return accordion
+
+def dict_from_accordion(accordion):
+    a = {x.description: x for x in accordion.children[0].children}
+    d = {x.description: x for x in accordion.children[1].children}
+    return a, d
