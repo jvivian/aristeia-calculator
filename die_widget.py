6d59b50fc519c4f28aab5eaca846fb3391d2feba
John Vivian <jtvivian@gmail.com>
2018-05-29 00:45:37 -0700

    PEP8

--- die_widget.py
+++ die_widget.py
@@ -1,18 +1,21 @@
-from ipywidgets import HBox, VBox, RadioButtons, Accordion
+from ipywidgets import HBox, RadioButtons, Accordion
 
 die_n = {'Black': 3,
-        'Orange': 3,
-        'Blue': 3,
-        'Red': 1,
-        'Green': 1,
-        'Yellow': 3}
+         'Orange': 3,
+         'Blue': 3,
+         'Red': 1,
+         'Green': 1,
+         'Yellow': 3}
+
 
 def create_die_widgets():
-    return  {x: RadioButtons(options=range(die_n[x] + 1), description=x) 
-             for x in die_n.keys()}
+    return {x: RadioButtons(options=range(die_n[x] + 1), description=x)
+            for x in die_n.keys()}
+
 
 def display_widgets(d):
     return HBox([y for x, y in sorted(d.iteritems())])
+
 
 def aristeia_dice_widget():
     a = create_die_widgets()
@@ -21,6 +24,7 @@ def aristeia_dice_widget():
     accordion.set_title(0, 'Attacker')
     accordion.set_title(1, 'Defender')
     return accordion
+
 
 def dict_from_accordion(accordion):
     a = {x.description: x for x in accordion.children[0].children}
